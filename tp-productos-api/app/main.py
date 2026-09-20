6455a589090ab3be5ed439f8aac51b14e452c0f4
Daniela Luz Belén Camacho <danuucamacho@gmail.com>
2026-09-20 18:08:55 -0300

    Agregue endpoint GET/ categorias

--- tp-productos-api/app/main.py
+++ tp-productos-api/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.productos.router import router as productos_router
 
-app = FastAPI()
+app = FastAPI(title="ProMadera - API de Productos")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
